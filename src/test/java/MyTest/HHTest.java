9d1c90070f3d9511776a8bb6053782f910dd6133
Alex <alex.zhidiaev@gmail.com>
2023-03-07 13:22:49 +0400

    fixed test to void, SearchPage

--- src/test/java/MyTest/HHTest.java
+++ src/test/java/MyTest/HHTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-import static MyTest.SearchPage.Vacancy;
 
 public class HHTest extends BaseSelenideTest {
     private final static String url = "https://hh.ru/";
@@ -16,22 +16,35 @@ public class HHTest extends BaseSelenideTest {
 
 
     @Test
-    public HashMap<String, Object> searchString() {
+    public void searchStringTest() {
         MainPage mainPage = new MainPage(url);
         mainPage.chooseCity("Москва");
         SearchPage searchPage = mainPage.search(SEARCH_STRING);
         searchPage.SetSalaryLimit(150000);
-        List<SelenideElement> vacancyList = (List<SelenideElement>) Vacancy;
+
+        List<SelenideElement> vacancyList = searchPage.vacancyList;
 
         HashMap<String, Object> result  = new HashMap<>();
-        Iterator<SelenideElement> iterator = vacancyList.iterator();
+
+        for(int i=0;i<vacancyList.size()-1;i++){
+            result.put("TITLE", searchPage.getTitle(i));
+            result.put("SALARY", searchPage.getSalary(i));
+            result.put("EMPLOYER", searchPage.getEmployer(i));;
+        }
+
+        /*Iterator<SelenideElement> iterator = vacancyList.iterator();
+
         while (iterator.hasNext()) {
             result.put("TITLE", searchPage.getTitle());
             result.put("SALARY", searchPage.getSalary());
             result.put("EMPLOYER", searchPage.getEmployer());;
-        }
-        System.out.println(result);
+        }*/
 
-        return result;
+        for(Map.Entry<String,Object> entry:result.entrySet()){
+            System.out.println(entry.getKey()+"  "+entry.getValue());
+        }
+
+
+
     }
 }
--- src/test/java/MyTest/SearchPage.java
+++ src/test/java/MyTest/SearchPage.java
@@ -3,30 +3,35 @@ package MyTest;
 import com.codeborne.selenide.SelenideElement;
 import core.BaseSeleniumPage;
 
+import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class SearchPage extends BaseSeleniumPage {
 
+    public List<SelenideElement> vacancyList = $$x("//div[@class='serp-item']");
     protected SelenideElement ExtendedSearch = $x("//a[@data-qa='advanced-search']");
     protected SelenideElement SetSalary = $x("//input[@data-qa='advanced-search-salary']");
     protected SelenideElement OnlyChosenSalarySearch = $x("//input[@data-qa='control-vacancysearch__only-with-compensation']/..");
     protected SelenideElement SearchButton = $x("//button[@data-qa='advanced-search-submit-button']");
-    protected static SelenideElement Vacancy = $x("//div[@class='serp-item']");
-    protected SelenideElement title = $x(Vacancy+"//h3");
+    /*protected SelenideElement title = $x(Vacancy+"//h3");
     protected SelenideElement salary = $x(Vacancy+"//span[@data-qa='vacancy-serp__vacancy-compensation']");
-    protected SelenideElement employer = $x(Vacancy+"//a[@data-qa='vacancy-serp__vacancy-employer']");
+    protected SelenideElement employer = $x(Vacancy+"//a[@data-qa='vacancy-serp__vacancy-employer']");*/
 
-    public String getTitle(){
-        return title.getText();
-    }
-    public String getSalary(){
-        return salary.getText();
-    }
-    public String getEmployer(){
-        return employer.getText();
+    public String getTitle(int index) {
+        return vacancyList.get(index).$x(".//h3").getText();
     }
 
-    public void SetSalaryLimit(Integer salary){
+    public String getSalary(int index) {
+        return vacancyList.get(index).$x(".//span[@data-qa='vacancy-serp__vacancy-compensation']").getText();
+    }
+
+    public String getEmployer(int index) {
+        return vacancyList.get(index).$x(".//a[@data-qa='vacancy-serp__vacancy-employer']").getText();
+    }
+
+    public void SetSalaryLimit(Integer salary) {
         ExtendedSearch.click();
         SetSalary.sendKeys(salary.toString());
         OnlyChosenSalarySearch.click();
